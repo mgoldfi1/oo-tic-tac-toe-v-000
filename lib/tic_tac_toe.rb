7ce660a509c64a5084b9cd09f04459e8086874fb
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-18 14:35:25 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -55,7 +55,7 @@ def turn_count
   @board.count{|token| token == "X" || token == "O"}
 end
 
-def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
+def current_player
+  turn_count(@board) % 2 == 0 ? "X" : "O"
 end
 end
