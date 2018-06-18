16b55f234c7744f0a103b170188df0db197730db
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-18 14:57:07 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -45,7 +45,7 @@ def turn
   index = input_to_index(user_input)
   if valid_move?(index)
     move(index, current_player(@board))
-    display_board(@board)
+    display_board
   else
     turn
   end
